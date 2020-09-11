003b9473a2e352e46ec80992a636d9d1ed6956a2
VovaSv <vovasvidi@gmail.com>
2020-09-11 23:34:25 +0300

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-    purge: [],
-    theme: {
-      extend: {},
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      }
     },
-    variants: {},
-    plugins: [],
-  }
+  },
+  variants: {},
+  plugins: [],
+}
